1047d40cf3888ccab7a28efa49ba44e5c9fc6bf4
Vinayak Kumawat <iamvinayakkt@gmail.com>
2024-01-15 01:10:00 +0530

    nav and hero update 1.01

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,14 @@ export default function Home() {
   return (
     <>
       <Navebar />
+
+      <section className="relative h-[50vh] w-screen border-b overflow-hidden">
+        <div className="absolute left-0 w-full h-full bg-[url('/assets/grid-bg.svg')] bg-cover bg-center opacity-40"></div>
+        <div className="relative flexCenter flex-col gap-2 h-full padding-container">
+          <h1 className="font-bold text-5xl lg:text-7xl">What I Know Is</h1>
+          <h2 className="font-bold text-3xl lg:text-5xl text-background bg-foreground px-4 py-2">BUZZ</h2>
+        </div>
+      </section>
     </>
   )
 }
--- components/Navbar/page.tsx
+++ components/Navbar/page.tsx
@@ -1,16 +1,24 @@
-import { GitHubLogoIcon } from '@radix-ui/react-icons'
+import { GitHubLogoIcon, HamburgerMenuIcon } from '@radix-ui/react-icons'
 import { NAV_LINKS } from "@/constants"
 import Link from "next/link"
 import { Button } from "../ui/button"
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
 
 const Navebar = () => {
     return (
-        <nav className='flexBetween padding-container'>
+        <nav className='flexBetween padding-container border-b'>
             <Link href={"/"}>
-                <span>Wikibuzz</span>
+                <span className='logo'>Wikibuzz.</span>
             </Link>
 
-            <ul>
+            <ul className='hidden lg:flex gap-6'>
                 {NAV_LINKS.map((link) => (
                     <Link href={link.href} key={link.key}>
                         <Button variant="link">{link.label}</Button>
@@ -18,12 +26,28 @@ const Navebar = () => {
                 ))}
             </ul>
 
-            <Link href={''}>
-            <Button>
-                <GitHubLogoIcon className="mr-2 h-4 w-4" />
-                Code
-            </Button>
+            <Link href={'https://github.com/vinayakkumawat/thewikibuzz'} target='_blank' className='hidden lg:flex'>
+                <Button>
+                    <GitHubLogoIcon className="mr-2 h-4 w-4" />
+                    Code
+                </Button>
             </Link>
+
+            <div className='lg:hidden flex'>
+                <Sheet>
+                    <SheetTrigger><HamburgerMenuIcon className='h-6 w-6' /></SheetTrigger>
+                    <SheetContent>
+                        <SheetHeader>
+                            <SheetTitle>Are you absolutely sure?</SheetTitle>
+                            <SheetDescription>
+                                This action cannot be undone. This will permanently delete your account
+                                and remove your data from our servers.
+                            </SheetDescription>
+                        </SheetHeader>
+                    </SheetContent>
+                </Sheet>
+            </div>
+
         </nav>
     )
 }
